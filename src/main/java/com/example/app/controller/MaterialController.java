889214a60ade013454f06d952648e4ad9de482db
eleganius <zhongguyajun@gmail.com>
2023-01-05 13:01:51 +0900

    add/edit-materialマッピング
    Materialクラス作成

--- src/main/java/com/example/app/controller/MaterialController.java
+++ src/main/java/com/example/app/controller/MaterialController.java
@@ -1,10 +1,15 @@
 package com.example.app.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.app.domain.Material;
 
 @Controller
 @RequestMapping("/admin/material") //URL
@@ -21,7 +26,48 @@ public class MaterialController {
 			Model model) {
 
 		return "admin/show-material";
-
 	}
+
+	@GetMapping("/add")
+	public String addGet() {
+		return "admin/add-material";
+	}
+
+	@PostMapping("/add")
+	public String addPost(
+			Material material,
+			HttpSession session) {
+
+		if(material.getName().isBlank()) {
+			return "admin/add-material";
+		}
+
+		session.setAttribute("material", material);
+
+		return "redirect:/admin/material/list";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String editGet(
+			@PathVariable Integer id,
+			Model model) {
+
+		return "admin/edit-material";
+	}
+
+	@PostMapping("/edit/{id}")
+	public String editPost(
+			Material material,
+			HttpSession session) {
+
+		if(material.getName().isBlank()) {
+			return "admin/edit-material";
+		}
+
+		session.setAttribute("material", material);
+
+		return "redirect:/admin/material/list";
+	}
+
 
 }
