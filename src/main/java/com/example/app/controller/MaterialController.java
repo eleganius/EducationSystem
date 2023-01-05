6b787ad3d7454b37991b9492dbd81af5e85e5ebf
eleganius <zhongguyajun@gmail.com>
2023-01-05 09:49:24 +0900

    show-materialマッピング

--- src/main/java/com/example/app/controller/MaterialController.java
+++ src/main/java/com/example/app/controller/MaterialController.java
@@ -1,7 +1,9 @@
 package com.example.app.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,6 +13,15 @@ public class MaterialController {
 	@GetMapping("/list") //URL
 	public String list() {
 		return "admin/list-material";//File
+	}
+
+	@GetMapping("/show/{id}")
+	public String show(
+			@PathVariable Integer id,
+			Model model) {
+
+		return "admin/show-material";
+
 	}
 
 }
